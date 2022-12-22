995e007e832ab469fbba83d251c3cfe04a5113c1
Ahmad Yones <ahmadmhdyones@gmail.com>
2022-12-23 01:25:50 +0300

    Add Laravel-Source-Encrypter
    - Install composer package
    - Add vscode preferences
    - Provide downloadable files in `/assets`
    - Configure Laravel-Source-Encrypter in project config.

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \sbamtr\LaravelSourceEncrypter\SourceEncryptServiceProvider::class,
 
         /*
          * Application Service Providers...
